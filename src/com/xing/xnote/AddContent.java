57110e5ef851556d933ee3c8dbd83dacd3b3ab0e
xiaoxiaoxing <18700936170@163.com>
2015-12-27 21:20:51 +0800

    增加存储图片功能

--- src/com/xing/xnote/AddContent.java
+++ src/com/xing/xnote/AddContent.java
@@ -1,12 +1,19 @@
 package com.xing.xnote;
 
+import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import android.app.Activity;
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -22,15 +29,36 @@ public class AddContent extends Activity implements OnClickListener{
 	private  ImageView ivVideo;
 	private  XnoteDbHelper dbHelper;
 	private  SQLiteDatabase db;
+	private  File imageFile;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.addcontent);
 		flag=getIntent().getStringExtra(MainActivity.FLAG);
 		initView();
+		setView();
 		dbHelper=new XnoteDbHelper(this);
 		db=dbHelper.getWritableDatabase();
 		
+	}
+	private void setView(){
+		if(flag.equals("1")){
+			ivImage.setVisibility(View.GONE);
+			ivVideo.setVisibility(View.GONE);
+		}else if(flag.equals("2")){
+			ivImage.setVisibility(View.VISIBLE);
+			ivVideo.setVisibility(View.GONE);
+			Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+			imageFile=new File(Environment.getExternalStorageDirectory().getAbsoluteFile()+"/"
+			                   +getTime()+".jpg");
+			intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(imageFile));
+			startActivityForResult(intent, 2);
+			
+		}else  if(flag.equals("3")){
+			
+			ivImage.setVisibility(View.GONE);
+			ivVideo.setVisibility(View.VISIBLE);
+		}
 	}
 	
 	private void initView(){
@@ -48,6 +76,7 @@ public class AddContent extends Activity implements OnClickListener{
 		ContentValues values=new ContentValues();
 		values.put(XnoteDbHelper.CONTENT, et.getText().toString());
 		values.put(XnoteDbHelper.TIME, getTime());
+		values.put(XnoteDbHelper.PATH, imageFile+"");
 		db.insert(XnoteDbHelper.TABLE_NAME, null, values);
 		
 	}
@@ -75,4 +104,16 @@ public class AddContent extends Activity implements OnClickListener{
 			break;
 		}
 	}
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+		super.onActivityResult(requestCode, resultCode, data);
+		if(requestCode==2){
+			
+			Bitmap bitmap=BitmapFactory.decodeFile(imageFile.getAbsolutePath());
+			ivImage.setImageBitmap(bitmap);
+			
+		}
+	}
+	
 }
--- src/com/xing/xnote/MyAdapter.java
+++ src/com/xing/xnote/MyAdapter.java
@@ -2,6 +2,9 @@ package com.xing.xnote;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.ThumbnailUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -58,10 +61,39 @@ public class MyAdapter extends BaseAdapter{
 		mCursor.moveToPosition(position);
 		String content=mCursor.getString(mCursor.getColumnIndex(XnoteDbHelper.CONTENT));
 		String time=mCursor.getString(mCursor.getColumnIndex(XnoteDbHelper.TIME));
+		String path=mCursor.getString(mCursor.getColumnIndex(XnoteDbHelper.PATH));
 		viewHolder.content.setText(content);
 		viewHolder.time.setText(time);
+		viewHolder.image.setImageBitmap(getImageThumbnail(path, 200, 200));
 		
 		return v;
+	}
+	public Bitmap getImageThumbnail(String uri,int width,int height){
+		
+		Bitmap bitmap=null;
+		BitmapFactory.Options options=new BitmapFactory.Options();
+		options.inJustDecodeBounds=true;
+		bitmap=BitmapFactory.decodeFile(uri);
+		options.inJustDecodeBounds=false;
+		int beWidth=options.outWidth/width;
+		int beHeight=options.outHeight/height;
+		int be=1;
+		if(beWidth<beHeight){
+			be=beWidth;
+		}
+		else{
+			be=beHeight;
+		}
+		if(be<0){
+			be=1;
+		}
+		options.inSampleSize=be;
+		bitmap=BitmapFactory.decodeFile(uri, options);
+		bitmap=ThumbnailUtils.extractThumbnail(bitmap, width, height, ThumbnailUtils.OPTIONS_RECYCLE_INPUT);
+		return bitmap;
+		
+		
+		
 	}
 	
 	class ViewHolder{
